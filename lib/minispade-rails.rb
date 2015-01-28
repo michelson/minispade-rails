e17195eddb41373de7a3eb12ff81c2e632345941
Miguel Michelson Martinez <miguelmichelson@gmail.com>
2015-01-28 11:58:40 -0300

    include Railtie id Rails defined

--- lib/minispade-rails.rb
+++ lib/minispade-rails.rb
@@ -4,7 +4,7 @@ require 'sprockets/engines'
 require "minispade-rails/config"
 require 'minispade-rails/directive_processor'
 require 'minispade-rails/compiler'
-require 'minispade-rails/railtie'
+require 'minispade-rails/railtie' if defined?(Rails)
 require "minispade-rails/version"
 
 module MinispadeRails
